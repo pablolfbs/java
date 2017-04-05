aba1c16348231ace55498befd5d002d75c747791
ads.2n <ads.2n@L606-16.senacrjedu.local>
2017-04-05 20:14:37 -0300

    gfh

---
+++ Teste1/src/static1/Main.java
@@ -0,0 +1,24 @@
+package static1;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Client c1 = new Client("Fabrício");
+		Client c2 = new Client("Claudio");
+		Client c3 = new Client();
+		System.out.println("c1: " + c1.getName());
+		System.out.println("c2: " + c2.getName());
+		
+		if(80 > Client.MIN_SCORE) {
+			//passou de fase
+		} else {
+			//continua tentando
+		}
+		
+		Client c = new Client();
+		System.out.println(c.getA());
+		System.out.println(c.isB());
+		System.out.println(c.getC());
+	}
+
+}

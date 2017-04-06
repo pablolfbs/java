fe507bd4a56e970c5123ccc9ced83e575addc4bc
ads.2n <ads.2n@L606-16.senacrjedu.local>
2017-04-05 22:01:48 -0300

    fsd

--- Teste1/src/Variavel_Constante.java
+++ Teste1/src/Variavel_Constante.java
@@ -1,21 +1,35 @@
 
 public class Variavel_Constante {
-	
+
 	public static void main(String[] args) {
-		int a = 10;
-		a = 20;
-		a = 30;
-		
-		final int B = 55;
+//		int a = 10;
+//		a = 20;
+//		a = 30;
+//		
+//		final int B = 55;
 //		B = 60; // Erro de compilação.
-		
-	}
-//	private void m1() {
+//
+//		private void m1() {
 //		int a = 10;
 //		{
-//			int b = 30;
+//		int b = 30;
 //		}
 //		System.out.println(b);
-//	}
-	
+//		}
+
+		int numbers[] = new int[10];
+		numbers[0] = 1;
+		numbers[1] = 2;
+		numbers[2] = 3;
+		numbers[3] = 4;
+		numbers[4] = 5;
+		numbers[5] = 6;
+		numbers[6] = 7;
+		numbers[7] = 8;
+		numbers[8] = 9;
+		numbers[9] = 10;
+		for (int number : numbers) {
+			System.out.println(number);
+		}
+	}
 }

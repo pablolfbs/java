ca1cdbe6af503fc281c91382e11fe40f32afd36e
ads.2n <ads.2n@L606-16.senacrjedu.local>
2017-04-12 22:12:15 -0300

    sdafds

---
+++ Teste1/src/heranca/Car.java
@@ -0,0 +1,20 @@
+package heranca;
+
+public class Car {
+	
+	private float accelaration;
+
+	public Car(float accelaration) {
+		super();
+		this.accelaration = accelaration;
+	}
+
+	public float getAccelaration() {
+		return accelaration;
+	}
+
+	public void setAccelaration(float accelaration) {
+		this.accelaration = accelaration;
+	}
+
+}
---
+++ Teste1/src/heranca/VolvoS60.java
@@ -0,0 +1,15 @@
+package heranca;
+
+public class VolvoS60 extends Car {
+	
+	public VolvoS60(float accelaration) {
+		super(accelaration);
+	}
+
+	public static void main(String[] args) {
+		
+//		VolvoS60 c = new VolvoS60();
+//		c.getAccelaration();
+	}
+	
+}

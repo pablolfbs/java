476c799b203ff54eedf058ce7210100cfe09a0d3
ads.2n <ads.2n@L606-16.senacrjedu.local>
2017-04-19 18:51:46 -0300

    jklçjlk

--- Teste1/src/heranca/VolvoS60.java
+++ Teste1/src/heranca/VolvoS60.java
@@ -2,14 +2,31 @@ package heranca;
 
 public class VolvoS60 extends Car {
 	
+	private boolean isTurbo = true;
+	
 	public VolvoS60(float accelaration) {
 		super(accelaration);
 	}
+	
+//	public float getAccelerationTurbo() {
+//		if(isTurbo) {
+//			return super.getAccelaration() * 1.4f;
+//		}
+//		return super.getAccelaration();
+//	}
+	
+	public float getAcceleration() {
+		if(isTurbo) {
+			return super.getAccelaration() * 1.4f;
+		}
+		return super.getAccelaration();
+	}
+
 
 	public static void main(String[] args) {
 		
-//		VolvoS60 c = new VolvoS60();
-//		c.getAccelaration();
+		VolvoS60 c = new VolvoS60(10);
+		c.getAccelaration();
 	}
 	
 }

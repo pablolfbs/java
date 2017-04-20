97843e6b4d4e3d914c3e4a40804097b028e7e33e
Pablo Faria <pablo.fariabritto@gmail.com>
2017-04-20 16:54:28 -0300

    fsasdf

--- Exercicios1/src/banco/Main.java
+++ Exercicios1/src/banco/Main.java
@@ -1,14 +1,20 @@
 
 package banco;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
 		Conta[] contas = new Conta[10];
 		for (int i = 0; i < contas.length; i++) {
+			
+			Scanner in = new Scanner(System.in);
+			
 			Conta c = new Conta();
-			c.setNumero("123-1");
+			c.setNumero(in.nextLine());
+			System.out.println(c.getNumero());
 		}
 		
 	}

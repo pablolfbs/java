e9f635370d4b737576e3fbc309fdc3f716e5e548
ads.2n <ads.2n@L606-16.senacrjedu.local>
2017-06-14 19:03:43 -0300

    fsdf

---
+++ Teste1/src/classe_interna/Main.java
@@ -0,0 +1,14 @@
+package classe_interna;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Person p = new Person();
+		p.new Address();
+		
+		new Person().new Address();
+		
+	}
+
+}
---
+++ Teste1/src/classe_interna/Person.java
@@ -0,0 +1,12 @@
+package classe_interna;
+
+public class Person {
+	
+	private Address address;
+
+	class Address {
+		private String country;
+		private String city;
+		
+	}
+}

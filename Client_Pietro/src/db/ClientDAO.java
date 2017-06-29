6cb2dca48979c53350d5b43d4ad1eb738e191076
ads.2n <ads.2n@L606-16.senacrjedu.local>
2017-06-28 21:29:51 -0300

    sfd

--- Client_Pietro/src/db/ClientDAO.java
+++ Client_Pietro/src/db/ClientDAO.java
@@ -2,7 +2,9 @@ package db;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import model.Client;
 
@@ -24,7 +26,69 @@ public class ClientDAO {
 		}
 	}
 
+	private static void update(Client client) {
+
+		Connection con = ConnectionFactory.getConnection();
+
+		try {
+			PreparedStatement command = con.prepareStatement("UPDATE client SET name = ? WHERE cpf = ?");
+			command.setString(1, client.getName());
+			command.setString(2, client.getCpf());
+			command.executeUpdate();
+			command.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void delete(Client client) {
+
+		Connection con = ConnectionFactory.getConnection();
+
+		try {
+			PreparedStatement command = con.prepareStatement("DELETE FROM client WHERE cpf = ?");
+			command.setString(1, client.getCpf());
+			command.executeUpdate();
+			command.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static ArrayList<Client> select() {
+		ArrayList<Client> select = new ArrayList<Client>();
+
+		Connection con = ConnectionFactory.getConnection();
+		try {
+			PreparedStatement command = con.prepareStatement("SELECT * FROM client");
+			ResultSet rs = command.executeQuery();
+			while (rs.next()) {
+				select.add(new Client(rs.getString("name"), rs.getString("cpf"), null));
+			}
+			rs.close();
+			command.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return select;
+	}
+
+	public static void Print(ArrayList<Client> list) {
+
+		for (Client client : list) {
+			System.out.println("CPF: " + client.getCpf() + "    " + "Nome: " + client.getName());
+		}
+	}
+
 	public static void main(String[] args) {
-		insert(new Client("Judas", "12345678901", null));
+		// insert(new Client("Judas", "12345678901", null));
+		// insert(new Client("Pablo", "12345678902", null));
+		// update(new Client("Pablo", "12345678901", null));
+		// delete(new Client("Judas", "12345678901", null));
+		// delete(new Client("Pablo", "12345678902", null));
+		Print(select());
 	}
 }

fa42fa10f62379f618b0dfd681df2cb98b941a65
Pablo <pablo.fariabritto@gmail.com>
2017-11-09 04:12:42 -0200

    updates

--- Client2/src/view/ClientDialog.java
+++ Client2/src/view/ClientDialog.java
@@ -2,4 +2,8 @@ package view;
 
 public class ClientDialog {
 
+	public ClientDialog(MainFrame mainFrame) {
+		// TODO Auto-generated constructor stub
+	}
+
 }
---
+++ Exercicios1/src/teste/Teste.java
@@ -0,0 +1,16 @@
+package teste;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			int matr = i + 1;
+			String matricula = Integer.toString(matr);
+			System.out.println(matricula);
+			System.out.println(matricula + 1);
+		}
+		
+	}
+
+}
